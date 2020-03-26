83f0076e331a2929ec9a980b06a503c41ff357ea
qianggezale <lqrj1012@sina.com>
2020-03-27 00:37:19 +0800

    定点同步config

--- cloud-config-bus-server-5010/src/main/java/cn/qiang/springcloud/AppConfigBusServer.java
+++ cloud-config-bus-server-5010/src/main/java/cn/qiang/springcloud/AppConfigBusServer.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer //开启配置中心 直接刷新总服务即可 例如：http://localhost:5010/actuator/bus-refresh
+//开启配置中心 直接刷新总服务即可 例如：http://localhost:5010/actuator/bus-refresh
+//定点同步中心下的某一个客户端修改配置 例如：http://localhost:5010/actuator/bus-refresh/cloud-config-bus-client:5020
+//http://链接/actuator/bus-refresh/spring.application.name:server.port
+@EnableConfigServer
 public class AppConfigBusServer {
     public static void main(String[] args) {
 
